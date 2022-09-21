5dfee0a5ba4f3b7dc5c14ffebad884bc835d55ff
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-21 07:35:45 -0700

    shorter is better

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
+    #if i < 10:
+    if i != 99:
         print("{:0>2d}".format(i), end=", ")
-    elif i >= 10:
-        if i != 99:
-            print("{0:d}".format(i), end=", ")
-        else:
-            print("{:d}".format(i))
+    else:
+        print("{0:d}".format(i))
