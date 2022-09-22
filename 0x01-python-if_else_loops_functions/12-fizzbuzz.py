a139675600ac3ed5c9b35882db26ef87211e3dcc
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-21 20:28:30 -0700

    following the std.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("{}".format("Fizz"), end=" ")
         elif i % 5 == 0 and i % 3 != 0:
             print("{}".format("Buzz"), end=" ")
-        elif i % 3 == 0 and i% 5 == 0:
+        elif i % 3 == 0 and i % 5 == 0:
             print("{}".format("FizzBuzz"), end=" ")
         else:
             print("{}".format(i), end=" ")
