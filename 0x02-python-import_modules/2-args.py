59abf3d4fb308bea9796a2174a58b11d36b24645
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-22 17:35:18 -0700

    this one took a while

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{} {}".format(len(sys.argv) - 1, "arguments."))
+    elif len(sys.argv) >= 2:
+        if len(sys.argv) == 2:
+            print("{} {}:".format(len(sys.argv) - 1, "argument"))
+            print("{}: {}".format(len(sys.argv) - 1, sys.argv[1]))
+        elif len(sys.argv) > 2:
+            print("{} {}:".format(len(sys.argv) - 1, "arguments"))
+            print("{}: {}".format(1, sys.argv[1]))
+            for w in range(3, len(sys.argv) + 1):
+                print("{}: {}".format(w - 1, sys.argv[w - 1]))
