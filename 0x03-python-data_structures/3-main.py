8b77328939f49cef8a44edeb1da28ecf0d538d76
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-26 21:14:01 +0000

    update

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [6, 7, 8, 9, 10]
+my_list = [6, 7, 8, 9, 10.7]
 print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,7 @@ def print_reversed_list_integer(my_list=[]):
    x = []
    x = reversed(my_list)
    for i in x:
-       print("{}".format(i))
+       if isinstance(i, float):
+           print("{:f}".format(i))
+       elif isinstance(i, int):
+           print("{:d}".format(i))
