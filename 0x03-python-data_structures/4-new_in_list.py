0b516ea5c8e4f7823f02706f4e3eada47f172058
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-26 21:44:03 +0000

    new update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    x = my_list.copy()
+    if idx < 0:
+        return x
+    elif idx > len(my_list):
+        return x
+    for i in range(len(my_list)):
+        if i == idx:
+            x[i] = element
+            return x
