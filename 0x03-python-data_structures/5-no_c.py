c1d140dc890f778747483342d69170be76302e66
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-27 14:55:12 +0000

    finally

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new = ""
-    for c in my_string:
-        if c not in ['c', 'C']:
-            new = new + c
-            return new
+    for st in my_string:
+        if st not in ["C", "c"]:
+            new = new + st
+    return new
