11dd28741c730261f12588c6eb8103caef865aa2
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-29 04:59:01 +0000

    rectified typo errors

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new = {key: value * 2 for value in a_dicitonary.values()}
+    new = {key: value * 2 for key, value in a_dictionary.items()}
     return new
