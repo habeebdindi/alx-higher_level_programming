409efb056463da61334c69ecdf042a812196ff3f
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-29 04:51:41 +0000

    newly added

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-        cpy = matrix.copy()
-        ret = []
-        fin = []
-        for i in cpy:
-            ret = list(map(lambda x: x ** 2, i))
-            fin.append(ret)
-        return fin
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
