9804c8c8f02b96a0fa6f3aaf34614f20170863e9
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-29 04:53:45 +0000

    update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new = {key: value * 2 for key, value in a_dicitonary.items()}
+    new = {key: value * 2 for value in a_dicitonary.values()}
     return new
