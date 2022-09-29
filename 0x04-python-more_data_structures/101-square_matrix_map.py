689fca02654a033b8edeaf904bc72d912aaf0bfd
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-29 04:09:21 +0000

    new update

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+        cpy = matrix.copy()
+        ret = []
+        fin = []
+        for i in cpy:
+            ret = list(map(lambda x: x ** 2, i))
+            fin.append(ret)
+        return fin
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,5 @@
 def only_diff_elements(set_1, set_2):
-    stp1 = set_1 - set_2
-    stp2 = set_2 - set_1
-    fin = stp1 | stp2
+    stp1 = set_1.difference(set_2)
+    stp2 = set_2.difference(set_1)
+    fin = stp1.union(stp2)
     return fin
