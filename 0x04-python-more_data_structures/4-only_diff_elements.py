9cc4e769c6d921827456e7d0e4d985e3fb95987b
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-09-29 04:12:35 +0000

    Corrected for symmetric difference

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,3 @@
 def only_diff_elements(set_1, set_2):
-    stp1 = set_1.difference(set_2)
-    stp2 = set_2.difference(set_1)
-    fin = stp1.union(stp2)
-    return fin
+    stp1 = set_1.symmetric_difference(set_2)
+    return stp1
