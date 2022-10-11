fdf7d51dcab8cf908226bf54ef7681d10b04af6f
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-10-11 03:59:18 +0000

    Changed to error handling

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        converted = int(value)
-        print("{:d}".format(converted))
+        print("{:d}".format(value))
         return True
     except ValueError:
         return False
