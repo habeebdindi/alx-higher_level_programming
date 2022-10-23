c28f3db07db1f2d84783b06e9cdfe076d2153e0a
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-10-23 18:56:19 +0000

    catch up with the past befor it catches up with you

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """class defining a square based on its size"""
+    def __init__(self, size):
+        """Initialises instance attributes
+        
+        Args:
+            size: this parameter holds the value of the size
+        """
+        self.__size = size
