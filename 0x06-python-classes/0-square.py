3a5e2960d8cabefe1495143c0f0353af5148b581
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-10-24 00:55:56 +0000

    Documentation updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module uses OOP and is not import guarded
+
+classes:
+    Square
+"""
+
+
 class Square:
-    """Empty class defining a square
-    
-    """
+    """Empty class defining a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+"""
+This module uses OOP and is not import guarded
+
+classes:
+    Square
+"""
+
+
 class Square:
-    """class defining a square based on its size
-    """
+    """class defining a square based on its size"""
     def __init__(self, size):
         """Initialises instance attributes
-        
+
         Args:
-            size: this parameter holds the value of the size
-        
+            size (int): single parameter which is the value of the \
+            size
+
+        Attributes:
+            size (int): private attribute that holds the value of \
+                        the size
         """
         self.__size = size
