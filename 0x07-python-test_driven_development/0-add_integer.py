480501d3cd3ea63637967ee892744fdb6d8c98de
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-01 13:58:30 +0000

    Test driven development is stressful but useful

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+
+    This module contains a function that adds two integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    This function adds two integers
+
+    Args:
+        a (int) or (float): 1st parameter
+        b (int) or (float), optional: 2nd paramter
+
+    Returns:
+        int: returns the integer sum of the two parameters
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return (a+b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+## THIS DIRECTORY CONTAINS ALL PROJECT TASKS ON Python - Test-driven development
