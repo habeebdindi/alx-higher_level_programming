5baa7a2357d58987ee0e1779d48fa1e9ab27cc2e
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-02 11:51:23 +0000

    code: heisenberg

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module prints your name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function prints:
+    "My name is <first name> <last name>"
+
+    Args:
+        first_name (str): a string containing your firstname
+        last_name (str): a string containing your last name
+
+    Returns:
+        a string containing your fullname
+    """
+    if not type(first_name) is str:
+        raise TypeError("first_name must be a string")
+    if not type(last_name) is str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name)) 
