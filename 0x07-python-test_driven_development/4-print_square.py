2b13cf73b5d490f1ce5812f8b658dc687386c6b8
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-02 13:33:23 +0000

    update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,6 +17,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
+        """raise TypeError("size must be an integer")"""
     for i in range(size):
         print("#" * size)
