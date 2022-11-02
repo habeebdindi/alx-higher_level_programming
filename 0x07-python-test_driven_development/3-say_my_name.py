9b079373c7761aa29984a6449a50821bc2c1ad90
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-02 11:52:37 +0000

    lost the trail

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -20,4 +20,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not type(last_name) is str:
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name)) 
+    print("My name is {} {}".format(first_name, last_name))
