335f8359241b7170d5d21a913ca4f432343c2b3a
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-03 08:44:36 +0000

    the fifth

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+function prints a text with 2 new lines after each of \
+these characters: ., ? and :
+"""
+
+def text_indentation(text):
+    """
+    Args:
+        text (str): the text to be checked and
+    """
+    if not type(text) is str:
+        raise TypeError("text must be a string")
+    t = text.replace('. ', ".\n\n").replace('? ', "?\n\n").replace(':', ":\n\n")
+    seperated = t.splitlines(True)
+    final_text = ""
+    for i in seperated:
+        final_text += i.strip(' ')
+    print(final_text, end="")
