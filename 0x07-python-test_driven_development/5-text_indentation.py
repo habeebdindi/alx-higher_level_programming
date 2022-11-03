814a911e653fe38dbabe148ad9e286a650482be6
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-03 08:58:07 +0000

    update src file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,6 +5,7 @@ function prints a text with 2 new lines after each of \
 these characters: ., ? and :
 """
 
+
 def text_indentation(text):
     """
     Args:
@@ -13,7 +14,7 @@ def text_indentation(text):
     if not type(text) is str:
         raise TypeError("text must be a string")
     text = text.replace('? ', "?\n\n").\
-replace(':', ":\n\n").replace('.', ".\n\n")
+        replace(':', ":\n\n").replace('.', ".\n\n")
     seperated = text.splitlines(True)
     final_text = ""
     for i in seperated:
