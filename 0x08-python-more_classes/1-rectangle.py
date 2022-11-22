5cdda1da4a519847321db228bdc4f9a4f64ffbe5
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-22 09:13:38 +0000

    added instances to the class, as well as documentation

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class that defines an object
+"""
+
+
+class Rectangle:
+    """
+    Attributes:
+        width (int): private instance attribute
+        height (int): private instance attribute
+    """
+    
+    def __init__(self, width=0, height=0):
+        """
+        Args:
+            width (int): width of the rectangle.
+            height (int): Height of the rectangle.
+
+        """
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        """
+        int: This defines a rectangle width, it can retrieve its value \
+             and set it.
+        """
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        int: This defines a rectangle height, it can retrieve its value \
+             and set it
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
