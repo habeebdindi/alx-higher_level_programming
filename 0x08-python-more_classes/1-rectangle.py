bd69fdd1a8ccc1b20590350c80039b7e20af9dde
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-22 09:40:38 +0000

    added excecptions to the init method and modified the main file for test

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
         width (int): private instance attribute
         height (int): private instance attribute
     """
-    
+
     def __init__(self, width=0, height=0):
         """
         Args:
@@ -18,9 +18,17 @@ class Rectangle:
             height (int): Height of the rectangle.
 
         """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif type(height) is not int:
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
-    
+
     @property
     def width(self):
         """
@@ -28,7 +36,7 @@ class Rectangle:
              and set it.
         """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
