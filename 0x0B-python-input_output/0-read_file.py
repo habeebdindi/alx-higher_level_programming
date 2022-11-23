88b2194ff1932894835b5cb9b46b77181064944c
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-23 21:45:44 +0000

    Starting off file Input/Ouutput

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains a function for reading a text file
+"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a text file and prints it to stdout
+
+    Args:
+        filename (str): name of the file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
