b8f64595a16f035bc972957fcf182371e2fc0f8c
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-12-03 02:47:20 +0000

    Calculating the avg of a field

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- script computes the score average of all records in the table second_table of the
 -- database hbtn_0c_0 in MySQL server.
-SELECT AVG(score) AS average FROM `secon_table`;
+SELECT AVG(score) AS average FROM `second_table`;
