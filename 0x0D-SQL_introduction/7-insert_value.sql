284e622b865740b6095f15fc72234908183f07f3
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-12-03 00:52:20 +0000

    fixed QUERY

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
 -- Inserts a new row in a table
-INSERT INTO first_table VALUES (89, "Holberton School");
+INSERT INTO first_table VALUES (89, "Best School");
