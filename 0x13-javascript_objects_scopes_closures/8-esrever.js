b5ef20b2133b95fd2a5cd1a16f3a758eb56da58c
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-12-21 05:39:26 +0000

    this is a test

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const nlist = [];
   for (let i = list.length; i >= 1; i--) {
-    nlist.push(list[i]);
+    nlist.push(list[i - 1]);
   }
   return (nlist);
 };
