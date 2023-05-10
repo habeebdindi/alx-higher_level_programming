79ee4580e80212ba0c0ec2c5976eff5f4f98cb2e
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-10 05:12:13 +0000

    Use print format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 8):
     for j in range(i + 1, 10):
-        print(f"{i:d}{j:d}", end=", ")
-print(f"89")
+        print("{:d}{:d}".format(i, j), end=", ")
+print("{}".format(89))
