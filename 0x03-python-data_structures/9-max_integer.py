77713b13c5e6d0727d74ab4be135a0ca44ff89b6
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-16 02:49:55 +0000

    Check list validity

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    max = 0
-    for num in my_list:
-        if num > max:
-            max = num
-    return max
+    if my_list:
+        if len(my_list) == 0:
+            return None
+        max = 0
+        for num in my_list:
+            if num > max:
+                max = num
+        return max
+    return None
