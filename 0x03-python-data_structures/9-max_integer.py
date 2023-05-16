acc4aabb8d65ef06df66b004842f906e79b73834
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-16 02:56:33 +0000

    Change the initial value of max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list:
         if len(my_list) == 0:
             return None
-        max = 0
+        max = my_list[0]
         for num in my_list:
             if num > max:
                 max = num
