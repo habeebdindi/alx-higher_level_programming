9013025ff4dc75907cd1a970345286bff4107e50
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-16 02:28:58 +0000

    Refactor code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     _list = []
-    _list[len(_list):] = my_string
-    if 'C' in _list:
-        _list.remove('C')
-    elif 'c' in _list:
-        _list.remove('c')
-    _str = ""
-    return _str.join(_list)
+    for ch in my_string:
+        if ch != 'C' and ch != 'c':
+            _list.append(ch)
+    string = "".join(_list)
+    return string
