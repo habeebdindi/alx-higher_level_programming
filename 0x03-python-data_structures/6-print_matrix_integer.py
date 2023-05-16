f8d6a577c1ae1feb9ceb81b8bbc6f529e0893c7b
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-16 02:30:21 +0000

    Add num formatter

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         i = 0
         while i < len(row):
-            print("{}".format(row[i]), end="")
+            print("{:d}".format(row[i]), end="")
             if i != len(row) - 1:
                 print(", ", end="")
             i += 1
