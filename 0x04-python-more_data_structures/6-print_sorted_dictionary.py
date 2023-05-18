6dfbce09fedeb32f336d6f7130d22db45fcb4368
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-18 02:21:23 +0000

    Add check for valid dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return None
     for k, v in sorted(a_dictionary.items()):
         print("{}: {}".format(k, v))
