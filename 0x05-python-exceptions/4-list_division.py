bb9699299e870e91c20c40b9f6223f704471d18c
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-30 01:55:21 +0000

    Remove type check

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new, typ = [], [int, float]
-    if len(my_list_1) <= 0 or len(my_list_2) <= 0 or list_length <= 0:
-        print("out of range")
-        return
+    new = []
     try:
         for i in range(list_length):
-            if type(my_list_1[i]) in typ and type(my_list_2[i]) in typ:
-                try:
-                    new.append(my_list_1[i] / my_list_2[i])
-                except (TypeError, ValueError) as ze:
-                    new.append(0)
-                    print("division by 0")
-            else:
+            try:
+                new.append(my_list_1[i] / my_list_2[i])
+            except ZeroDivisionError as ze:
+                print("division by 0")
+                new.append(0)
+            except (TypeError, ValueError):
                 new.append(0)
                 print("wrong type")
-    except (TypeError, ValueError) as e:
+            except IndexError:
+                new.append(0)
+                print("out of range")
+    except Exception as e:
         pass
     finally:
         return new
