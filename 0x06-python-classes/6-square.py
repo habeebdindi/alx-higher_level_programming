d713eb4b630b0d274a680c0494b981409a58475e
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-31 02:24:28 +0000

    Use isinstance again

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -89,9 +89,9 @@ class Square:
         try:
             if not isinstance(value, tuple) or len(value) != 2:
                 raise TypeError
-            if not isinstance(value[0], int) is not int:
+            if not isinstance(value[0], int):
                 raise TypeError
-            if not isinstance(value[1], int) not int:
+            if not isinstance(value[1], int):
                 raise TypeError
             if value[0] < 0 or value[1] < 0:
                 raise TypeError
