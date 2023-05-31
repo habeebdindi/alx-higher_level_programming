85619e1a524d37e584bf93138d93b40241bb7cfe
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-31 01:14:45 +0000

    Change insinstance to type()

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -83,7 +83,7 @@ class Square:
         """
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
