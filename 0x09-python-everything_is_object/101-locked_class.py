172da7f42eed0bb8aabb3ede704845c9b8c8b1a7
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-09 15:55:35 +0000

    Add documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+Contains one class
+"""
+
+
 class LockedClass:
+    """
+    Allows access to only a specific instance
+    """
     def __setattr__(self, name, value):
+        """
+        sets an instance attribute if only it's name is first_name
+        """
         if not hasattr(self, name) and name != "first_name":
             raise AttributeError
         super().__setattr__(name, value)
