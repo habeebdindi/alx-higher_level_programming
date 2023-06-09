53cd37bed7e39c98533921877187280d435eee0b
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-09 16:31:35 +0000

    Use an obj subclass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,15 +4,18 @@ Contains one class
 """
 
 
-class LockedClass:
+class LockedClass(object):
     """
     Allows access to only a specific instance
     """
+
+    __slots__ = ['first_name']
+
     def __setattr__(self, name, value):
         """
         sets an instance attribute if only it's name is first_name
         """
-        if not hasattr(self, name) and name != "first_name":
+        if name not in self.__slots__:
             raise AttributeError("'LockedClass' object has no attribute '{}'".
                                  format(name))
         super().__setattr__(name, value)
