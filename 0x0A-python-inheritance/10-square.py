175bad84bb509d6e976a3d0386a8eace2e086caa
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-13 05:15:01 +0000

    Bring import below doc

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
 Contains a class that inherits from Rectangle
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 Contains a class that inherits from base_geometry
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
