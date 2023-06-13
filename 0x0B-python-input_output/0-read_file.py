66329349f2a43c34361fc33afc29c8821d36255a
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-13 21:59:55 +0000

    Change approach

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,5 @@
 def read_file(filename=""):
     """ function reads a text file in UTF8 encoding """
     with open(filename, mode='r', encoding="utf-8") as f:
-        f.seek(0)
-        contents = f.read()
-        print(contents)
-    return contents
+        for line in f:
+            print(line, end='')
