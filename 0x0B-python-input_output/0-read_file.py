bc2bea1845d321d4682abacd3e39aa0747abb821
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-13 21:55:17 +0000

    Update file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,6 @@
 def read_file(filename=""):
     """ function reads a text file in UTF8 encoding """
     with open(filename, mode='r', encoding="utf-8") as f:
-        print(f.read())
+        contents = f.read()
+        print(contents)
+    return contents
