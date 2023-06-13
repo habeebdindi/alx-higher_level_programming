6421923b819bf9849f1a45e3b121adcd1a19d468
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-13 01:58:18 +0000

    Update doc

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
+This module contains a single class
 """
 
 
 class MyList(list):
     """
+    This class contains a single method
     """
     def print_sorted(self):
         """
+        This method prints an inherited list attribute in asc order
         """
         print(sorted(self))
