74e0b927ef507a516f06bcc425946e5cfc96db51
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-08-15 02:00:43 +0000

    Refactor code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,16 +2,15 @@
 const args = process.argv.slice(2);
 
 if (args.length <= 1) {
+  console.log(0);
+} else {
+  const numbers = args.map(arg => parseInt(arg));
+  const uniqueNumbers = Array.from(new Set(numbers));
+
+  if (uniqueNumbers.length <= 1) {
     console.log(0);
+  } else {
+    const sortedNumbers = uniqueNumbers.sort((a, b) => b - a);
+    console.log(sortedNumbers[1]);
+  }
 }
-
-const numbers = args.map(arg => parseInt(arg));
-const uniqueNumbers = Array.from(new Set(numbers));
-
-if (uniqueNumbers.length <= 1) {
-    console.log(0);
-    return;
-}
-
-const sortedNumbers = uniqueNumbers.sort((a, b) => b - a);
-console.log(sortedNumbers[1]);
