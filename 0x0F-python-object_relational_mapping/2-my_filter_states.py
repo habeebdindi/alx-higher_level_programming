a60ceeedc14674e7b39a6d19a959f0ca071b039e
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-08-25 16:55:32 +0000

    Use .format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,8 +23,9 @@ def main():
         db=database,
         charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE BINARY name LIKE %s ORDER BY id ASC"
-    cur.execute(query, (match + '%',))
+    query = "SELECT * FROM states
+    WHERE name LIKE '{}%' ORDER BY id ASC".format(match)
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
