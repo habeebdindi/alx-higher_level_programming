71d8648bc90e973762e9ab1ddb6b075c3a340c84
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-08-25 16:14:25 +0000

    Add some basic scripts

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script lists all states from the db hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    function to that runs everything
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script lists state that that matches a string from the db hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    function to that runs everything
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    match = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE NAME LIKE %s", (match + '%',))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
