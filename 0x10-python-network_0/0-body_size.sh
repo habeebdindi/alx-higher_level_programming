9800d6b6fc985d90720c293ad244ef2181cd5f3a
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-01 01:46:31 +0000

    Cleand directory

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-url="$1"
-curl -sI url | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r '
+curl -sI http://globallearna.tech | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r '
