0e5e39d8187e80bcdc5a8439fb9eefd03afd4f4e
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-01 01:57:03 +0000

    Update script to be dynamic

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -sI http://globallearna.tech | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r '
+url="$1"
+curl -sI url | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r '
