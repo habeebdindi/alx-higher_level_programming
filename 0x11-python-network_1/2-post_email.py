0e96eb1e8b40531b7f450cfc0e98e7428ab53205
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-02 01:21:28 +0000

    Add task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    param = {'email': email}
+    data = urllib.parse.urlencode(param)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        returned = response.read()
+    print(returned)
