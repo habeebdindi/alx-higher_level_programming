7930e46cd0742812035c4a6afa78e3c7bdc09c1c
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-23 21:24:36 +0000

    Add task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,14 +2,9 @@
 """
 Now using ORM for abstraction
 """
-
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
-
-engine = create_engine(
-    'mysql+mysqldb://root:khalEXX001$@localhost:3306/hbtn_0e_4_usa')
-connection = engine.connect()
 Base = declarative_base()
 
 
@@ -20,12 +15,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-"""
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-session.close()
-"""
