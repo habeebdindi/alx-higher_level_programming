20551ad5bf762f1666a04da44206b8638846c6b8
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-24 02:02:26 +0000

    Fix style

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 from sqlalchemy.orm import relationship
 
+
 class City(Base):
     """City class definition
     """
