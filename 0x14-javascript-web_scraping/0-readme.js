d6a76b87bf6bc04083b4f0ab02da43844cf62c59
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-27 02:19:05 +0000

    Add task one

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (error, data) => {
+    if (error) {
+	console.log(error);
+    } else {
+	console.log(data);
+    }
+});
