bf192228df835e7d084b3b0b1a908e2e47ed490d
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-09-27 02:38:14 +0000

    Add task 3

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url).on('response', (response) => {
+  console.log('code: ' + response.statusCode);
+});
