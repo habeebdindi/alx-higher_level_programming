1fd506ccf5536bf707ad115329708918df6069cb
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-22 02:37:02 +0100

    Fix pycodestyle

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -19,24 +19,25 @@ def print_codes(codes, file_size):
 try:
     for line in sys.stdin:
         loops += 1
+        split = line.strip().split()
 
-        split = line.split()
-        if len(split) != 9:
-            continue
-        if len(split[0].split(".")) != 4:
-            continue
+        try:
+            status = int(split[-2])
+            if status in codes:
+                count[status] += 1
+        except Exception:
+            pass
 
-        status = int(split[7])
-        if status in codes:
-            count[status] += 1
-
-        total += int(split[8])
+        try:
+            total += int(split[-1])
+        except Exception:
+            pass
 
         if loops == 10:
             loops = 0
             print_codes(count, total)
-except (KeyboardInterrupt, TypeError) as e:
+except (KeyboardInterrupt, ValueError) as e:
     print_codes(count, total)
-    print(e)
+    raise
 
 print_codes(count, total)
